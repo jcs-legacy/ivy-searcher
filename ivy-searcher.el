42f1d2db5e07a179168338859473104a2bd437d6
jenchieh <jcs090218@gmail.com>
2020-06-21 20:58:57 +0800

    Mark version.

--- ivy-searcher.el
+++ ivy-searcher.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Ivy interface to use searcher.
 ;; Keyword: ivy interface use searcher search
-;; Version: 0.1.1
+;; Version: 0.1.
 ;; Package-Requires: ((emacs "25.1") (ivy "0.8.0") (searcher "0.1.0") (s "1.12.0") (f "0.20.0"))
 ;; URL: https://github.com/jcs-elpa/ivy-searcher
 
